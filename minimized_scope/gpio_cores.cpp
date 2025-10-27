f4f6eecfb48144e957ec1d4c957cc2af5367ceb6
cm1202 <96462904+cm1202@users.noreply.github.com>
2025-10-27 14:15:40 -0500

    Update gpio_cores.cpp

--- minimized_scope/gpio_cores.cpp
+++ minimized_scope/gpio_cores.cpp
@@ -8,6 +8,7 @@
  ********************************************************************/
 
 #include "gpio_cores.h"
+#include <sys/_stdint.h>
 
 /**********************************************************************
  * GpiCore
@@ -74,6 +75,11 @@ void GpoCore::write(int bit_value, int bit_pos) {
    bit_write(wr_data, bit_pos, bit_value);
    io_write(base_addr, DATA_REG, wr_data);
 }
+void GpoCore::write_s(uint16_t speed) {
+   io_write(base_addr, SPEED_REG, speed);
+}
+ 
+
 
 /**********************************************************************
  * PwmCore
