5313d4adcfe6c1f4c6f5e804768cfdb6d20e70c3
cm1202 <96462904+cm1202@users.noreply.github.com>
2025-10-27 14:27:54 -0500

    Add files via upload

--- multiple_en/chu_gpo.sv
+++ multiple_en/chu_gpo.sv
@@ -16,36 +16,28 @@ module chu_gpo
 
    // declaration
    logic [W-1:0] buf_reg;
-   logic wr_en,wr_D,wr_S;
+   logic wr_en,wr_D;
 
    // body
    // output buffer register
-   always_ff @(posedge clk, posedge reset)
-      if (reset)
+   always_ff @(posedge clk, posedge reset) begin
+      if (reset)begin
          buf_reg <= 0;
-      else   
-         if (wr_en) begin
-            buf_reg <= wr_data[W-1:0];
-            end
-         else if(wr_D)begin
-            buf_reg <= wr_data[W-1:0];
-            end 
-         else if (wr_S)begin 
-             buf_reg <= wr_data[W-1:0];
-             end 
+      end
+      else if (wr_D)begin
+          buf_reg <= wr_data[W-1:0];
+      end 
+   end 
       
-   // decoding logic 
-   assign wr_en = cs && write;
+   
    assign wr_D = cs && write && (addr == 5'b00000); 
-   assign wr_S = cs && write && (addr == 5'b00001);
    //assign wr_en = cs && write; 
-   // slot read interface
+
+
    assign rd_data =  0;
-   // external output
-   //assign dout[0] = 1'b1;   
+
    assign dout = buf_reg;
 endmodule
        
-
 
 
